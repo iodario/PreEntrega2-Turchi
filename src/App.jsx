014fdb22ae3b5901b79e522d512510fb3e3f3c0a
Dario Turchi <iodariomdq@outlook.com>
2022-10-12 23:16:51 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import './App.css'
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 
-
 function App() {
-  
-
   return (
     < >
-      <NavBar/>
-      <ItemListContainer greeting='Welcome to our store !'/>
+      <NavBar />
+      <ItemListContainer greeting='Welcome to our store !' />
     </>
   )
 }
